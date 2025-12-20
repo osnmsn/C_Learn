ad3f64341026c25d0817d3e9fb1ce372062c74fc
osnmsn <dewajji407@gmail.com>
2025-12-20 17:48:52 -0600

    Delete EngineA1.0.0 directory

--- EngineA1.0.0/Input_engine.h
+++
@@ -1,7 +0,0 @@
-#ifndef INPUT_ENGINE_H
-#define INPUT_ENGINE_H
-
-// 获取键盘输入，如果没有返回0
-int getInput(void);
-
-#endif
--- EngineA1.0.0/Render_engine.h
+++
@@ -1,8 +0,0 @@
-#ifndef RENDER_ENGINE_H
-#define RENDER_ENGINE_H
-
-extern int player_x;   // 声明全局变量
-void initRender(void);
-void draw(void);
-
-#endif
